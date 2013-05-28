c688c409de7922b5a24d59c04ada5ba4c5af74a8
Curtis Badke <curtis@badkid.ca>
2013-05-28 10:49:42 -0600

    Add install packages for Windows ADK

---
+++ windows_adk/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+$packageName = "WindowsADK.WinPE"
+# the $version variable below is automatically set by our update script
+# we hardcode the version in this script, so that users can install specific
+# versions from Chocolatey.org
+#
+$url = "http://download.microsoft.com/download/9/9/F/99F5E440-5EB5-4952-9935-B99662C3DF70/adk/adksetup.exe"
+
+$silentArgs = "/quiet /norestart /log $env:temp\win_adk.log"
+
+try { 
+        Install-ChocolateyPackage $packageName 'exe' $silentArgs $url
+
+        Write-ChocolateySuccess "$packageName"
+} catch {
+        Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
+        throw
+}
