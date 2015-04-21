0a571f402832e1c041675987b23b09dd9d13b517
Curtis Badke <curtis.badke@devfacto.com>
2015-04-21 11:15:51 -0600

    Switch name of msi found in zip. Apparently Jenkins changed their naming pattern

--- jenkins/tools/chocolateyInstall.ps1
+++ jenkins/tools/chocolateyInstall.ps1
@@ -4,25 +4,25 @@ $packageName = "jenkins"
 # versions from Chocolatey.org
 $version = "1.610"
 $zipFile = "jenkins-$version.zip"
-$msiFile = "jenkins-$version.msi"
+$msiFile = "jenkins.msi"
 $url = "http://mirrors.jenkins-ci.org/windows/$zipFile"
 $url64 = $url
 $silentArgs = "/quiet"
 $validExitCodes = @(0)
 
-try { 
+try {
         $chocTempDir = Join-Path $env:TEMP "chocolatey"
         $tempDir = Join-Path $chocTempDir "$packageName"
         if (![System.IO.Directory]::Exists($tempDir)) {[System.IO.Directory]::CreateDirectory($tempDir)}
         $file = Join-Path $tempDir $zipFile
-                
+
         Get-ChocolateyWebFile $packageName $file $url $url64
         Get-ChocolateyUnzip $file $tempDir "" $packageName
 
         $file = Join-Path $tempDir $msiFile
 
         Install-ChocolateyInstallPackage $packageName 'msi' $silentArgs $file -validExitCodes $validExitCodes
-  
+
         Write-ChocolateySuccess "$packageName"
 } catch {
         Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
