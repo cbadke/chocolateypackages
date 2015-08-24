9d981bf9a5b139b88fa3e05b9e5433e18f93b823
Curtis Badke <curtis.badke@devfacto.com>
2015-08-24 14:19:21 -0600

    Upgrade windows-adk for Windows 10 version

--- windows_adk/tools/chocolateyInstall.ps1
+++ windows_adk/tools/chocolateyInstall.ps1
@@ -3,11 +3,11 @@ $packageName = "WindowsADK.WinPE"
 # we hardcode the version in this script, so that users can install specific
 # versions from Chocolatey.org
 #
-$url = "http://download.microsoft.com/download/6/A/E/6AEA92B0-A412-4622-983E-5B305D2EBE56/adk/adksetup.exe"
+$url = "http://download.microsoft.com/download/0/A/A/0AA382BA-48B4-40F6-8DD0-BEBB48B6AC18/adk/adksetup.exe"
 
 $silentArgs = "/quiet /norestart /log $env:temp\win_adk.log"
 
-try { 
+try {
         Install-ChocolateyPackage $packageName 'exe' $silentArgs $url
 } catch {
         Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
