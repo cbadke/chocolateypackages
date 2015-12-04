05818bed7ef98c8e3ed4da14ffdb6bef117857cc
Curtis Badke <curtis.badke@devfacto.com>
2015-12-03 17:56:45 -0700

    update adk for 10.1.10586

--- windows_adk_winpe/tools/chocolateyInstall.ps1
+++ windows_adk_winpe/tools/chocolateyInstall.ps1
@@ -3,7 +3,7 @@ $packageName = "WindowsADK.WinPE"
 # we hardcode the version in this script, so that users can install specific
 # versions from Chocolatey.org
 #
-$url = "http://download.microsoft.com/download/0/A/A/0AA382BA-48B4-40F6-8DD0-BEBB48B6AC18/adk/adksetup.exe"
+$url = "http://download.microsoft.com/download/3/8/B/38BBCA6A-ADC9-4245-BCD8-DAA136F63C8B/adk/adksetup.exe"
 
 $silentArgs = "/quiet /norestart /log $env:temp\win_adk.log /features OptionId.DeploymentTools OptionId.WindowsPreinstallationEnvironment"
 
