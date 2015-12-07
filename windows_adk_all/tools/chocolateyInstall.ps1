2333cfd83b97f55abaf8ed30dbe548c35ffd7f51
Curtis Badke <curtis.badke@devfacto.com>
2015-12-07 10:00:02 -0700

    Updated Jenkins package to version 1.640

--- windows_adk_all/tools/chocolateyInstall.ps1
+++ windows_adk_all/tools/chocolateyInstall.ps1
@@ -3,7 +3,7 @@ $packageName = "WindowsADK.WinPE"
 # we hardcode the version in this script, so that users can install specific
 # versions from Chocolatey.org
 #
-$url = "http://download.microsoft.com/download/3/8/B/38BBCA6A-ADC9-4245-BCD8-DAA136F63C8B/adk/adksetup.exe"
+$url = "http://download.microsoft.com/download/8/1/9/8197FEB9-FABE-48FD-A537-7D8709586715/adk/adksetup.exe"
 
 $silentArgs = "/quiet /norestart /log $env:temp\win_adk.log /features +"
 
